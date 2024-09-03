4a0fcbb9cdeb5ea313a60b800c12ba0cc7b4ca0b
AprendizEl <aprendiz3@controldiseno.com>
2024-09-03 07:34:16 -0500

    dio mio

--- installer/inst/inst/obj/Debug/net8.0-windows/inst.AssemblyInfo.cs
+++ installer/inst/inst/obj/Debug/net8.0-windows/inst.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("inst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48813cd591dd7fa3ef0a19d07b559d64f5abb71a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de166257bd40b0629abc8747d4f6db46ffb1bf02")]
 [assembly: System.Reflection.AssemblyProductAttribute("inst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("inst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
